545845b1c845f06bc794be072339fb0ba9f2a29b
Devin Nguyen <apptesterr@gmail.com>
2011-09-15 07:45:16 -0700

    Session code

--- Front-end/dashboard.php
+++ Front-end/dashboard.php
@@ -1,4 +1,10 @@
-<?php session_start()?>
+<?php
+session_start();
+if (!(isset($_SESSION['robo']))) {
+  header('Location: http://cytopic.net/robotics/index.php');
+  exit;
+  }
+?>
 <!DOCTYPE html>
 <head>
 	<meta charset="utf-8">
--- Front-end/index.php
+++ Front-end/index.php
@@ -1,3 +1,47 @@
+<?php
+$db_user = "yroot";
+$db_pass = "cytopic";
+$db_databse = "test";
+$db_host = "mysql";
+$db_connect = mysql_connect ($db_host, $db_user, $db_pass);
+$db_select = mysql_select_db ($db_database);
+
+function form($data) { // Prevents SQL Injection
+   global $db_connect;
+   $data = ereg_replace("[\'\")(;|`,<>]", "", $data);
+   $data = mysql_real_escape_string(trim($data), $db_connect);
+   return stripslashes($data);
+}
+
+session_start();
+if ((isset($_SESSION['robo']))) {
+  header('Location: http://cytopic.net/robotics/dashboard.php');
+}
+
+if(isset($_POST['login'])) {
+	if(isset($_POST['pwd'])) {
+		if(isset($_POST['username'])) {
+			$md5_password = md5($_POST['pwd']);
+			$q = mysql_query("SELECT * FROM RoboUsers WHERE Username='$username_login'");
+			$r = mysql_num_rows($q);
+			if($r > 0) {
+				$result = mysql_query("SELECT password FROM RoboUsers WHERE UserPassword='$username_login'");
+				$result2 = mysql_fetch_array($result);
+				$database_password = $result2['password'];
+				if($md5_password == $database_password) {
+					$_SESSION['robo'] = "$username_login";
+					header("Location: http://cytopic.net/robotics/dashboard.php");
+					break;
+				} else {
+					$true_false = true;
+				}
+			} else {
+				$other_true = true;
+			}
+		}
+	}
+}
+?>
 <!doctype html>
 <head>
 	<meta charset="utf-8">
